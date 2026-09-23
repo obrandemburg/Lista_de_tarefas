9b18d56bbc4eeb52c1cefb7ef01def0fe715c46f
Matheus <gdmatheus12@gmail.com>
2026-09-23 13:47:59 -0300

    Cria estrutura inicial de controller para Tarefas

---
+++ Controllers/TarefaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lista_de_tarefas.Controllers
+{
+    public class TarefaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
